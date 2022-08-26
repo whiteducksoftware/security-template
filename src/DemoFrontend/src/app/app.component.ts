f5fedbfd2a7d60492b61757adc1c11404343cdc0
Stefan Kürzeder <info@stivik.me>
2022-08-26 16:03:08 +0200

    Added vulnerabilities

--- src/DemoFrontend/src/app/app.component.ts
+++ src/DemoFrontend/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DemoFrontend';
+
+  endsWith(x: string, y: string) {
+    let index = x.lastIndexOf(y);
+    return x.lastIndexOf(y) === x.length - y.length;
+  }
 }
